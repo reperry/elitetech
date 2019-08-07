68de37670bbca347fb8d52452b1caa1a9591264e
haena <hsong36@asu.edu>
2019-08-06 18:51:19 -0700

    Added LogOff Function

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -261,9 +261,6 @@ namespace ProjectTemplate
         [WebMethod(EnableSession = true)]
         public bool LogOff()
         {
-            //if they log off, then we remove the session.  That way, if they access
-            //again later they have to log back on in order for their ID to be back
-            //in the session!
             Session.Abandon();
             return true;
         }
