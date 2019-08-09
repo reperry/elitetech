bf4522bdc94f2f29dc391042d5ac9035046d0b44
tmhamre <49290931+tmhamre@users.noreply.github.com>
2019-08-09 15:27:39 -0700

    added post class for data

---
+++ ProjectTemplate/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Post
+    {
+        public string postid;
+        public string content;
+        public int likes;
+        public string timestamp;
+        public string user;
+    }
+}
