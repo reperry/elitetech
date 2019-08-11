0828e6881bc40f145ba0162bad865c0bf6a2673d
tmhamre <49290931+tmhamre@users.noreply.github.com>
2019-08-10 17:33:32 -0700

    Add files via upload

---
+++ ProjectTemplate/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class User
+    {
+        public string userid;
+        public string username;
+        public bool isadmin;
+    }
+}
